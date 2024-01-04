5f1a1968fd29afc72b95e2739a2f133e47c1779b
Sebastian Gonzalez <jusegofo@gmail.com>
2024-01-03 23:43:28 -0500

    addign components to the router

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
@@ -9,13 +12,18 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
   declarations: [
     AboutPageComponent,
     HomePageComponent,
+    SideBarComponent,
+    ContactPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
     HomePageComponent,
+    SideBarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
